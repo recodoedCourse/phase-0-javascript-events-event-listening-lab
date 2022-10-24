daf154c2f3ab64c6047750a1e71bb618716cc5c8
HomeLanderz <86560837+HomeLanderz@users.noreply.github.com>
2022-10-24 11:07:26 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const input = document.querySelector('input#button');
 function addingEventListener() {
+    input.addEventListener('click', function() {
+    console.log('I was clicked!');
+});
 }
+
+
